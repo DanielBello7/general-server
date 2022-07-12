dc4fad3899dca4640265fc98222efd568ab63cc6
Daniel Bello <gokebello@gmail.com>
2022-07-12 20:18:28 +0100

    removed the localhost address

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ const server = http.createServer(app);
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")));
 
 const address = "localhost";
 const port = 3400;
 
 app.get('/', (req, res) => {
-	return res.send('home page');
+   return res.send('home page');
 });
 
 
-server.listen(port, address, () => {
+server.listen(port, () => {
    console.log(`server running on http://${address}:${port}.....`);
 });
